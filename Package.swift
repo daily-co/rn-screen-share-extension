5cf4de47534e4caed43c8d9f007f45837f771459
Filipi Fuchter <filipi87@gmail.com>
2023-10-02 10:27:55 -0300

    Fixing swift manifest

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "ReactNativeDailyJSScreenShareExtension",
             url: "https://www.daily.co/sdk/ReactNativeDailyJSScreenShareExtension.xcframework-0.0.1.zip",
-            // path: "./dist/ReactNativeDailyJSScreenShareExtension.xcframework.zip"
+            checksum: "ea29d0fa3f544a541f7d429a3ec792a1320f98e099892d0cb964ac93d167f00a"
         )
     ]
 )
